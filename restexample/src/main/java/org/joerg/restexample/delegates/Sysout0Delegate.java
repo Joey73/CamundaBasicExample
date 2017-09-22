890610edbf4a87e82a9fee151c5c2098ee90b90b
Joey73 <schmitz873@gmail.com>
2017-09-22 14:59:24 +0200

    Just delete an empty line.

--- restexample/src/main/java/org/joerg/restexample/delegates/Sysout0Delegate.java
+++ restexample/src/main/java/org/joerg/restexample/delegates/Sysout0Delegate.java
@@ -4,7 +4,6 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 public class Sysout0Delegate implements JavaDelegate {
-
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		System.out.println("Sysout0Delegate is called with variables " + execution.getVariables());
--- restexample/src/main/java/org/joerg/restexample/delegates/Sysout10Delegate.java
+++ restexample/src/main/java/org/joerg/restexample/delegates/Sysout10Delegate.java
@@ -4,7 +4,6 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 public class Sysout10Delegate implements JavaDelegate {
-
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		System.out.println("Sysout10Delegate is called with variables " + execution.getVariables());
