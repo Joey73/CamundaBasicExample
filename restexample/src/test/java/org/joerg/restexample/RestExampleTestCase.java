bbf612fab09d2d9623c6f3f9aac9d5ca6a807aad
Joey73 <schmitz873@gmail.com>
2017-09-22 17:08:13 +0200

    Added JUnit tests

---
+++ restexample/src/test/java/org/joerg/restexample/RestExampleTestCase.java
@@ -0,0 +1,52 @@
+package org.joerg.restexample;
+
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineTestCase;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+
+public class RestExampleTestCase extends ProcessEngineTestCase {
+
+	@Deployment(resources = { "restexample.bpmn" })
+	public void testHappyPath1() {
+		// Start process
+		ProcessInstance pi = runtimeService.startProcessInstanceByKey("process");
+
+		// Get first task
+		Task task = taskService.createTaskQuery().processInstanceId(pi.getId()).singleResult();
+		assertEquals("UserTask1", task.getTaskDefinitionKey());
+
+		// Set variable
+		VariableMap variables = Variables.createVariables();
+		variables.clear();
+		variables.put("FormFieldX", 7L);
+
+		// Complete task
+		taskService.complete(task.getId(), variables);
+
+		assertProcessEnded(pi.getId());
+	}
+
+	@Deployment(resources = { "restexample.bpmn" })
+	public void testHappyPath2() {
+		// Start process
+		ProcessInstance pi = runtimeService.startProcessInstanceByKey("process");
+
+		// Get first task
+		Task task = taskService.createTaskQuery().processInstanceId(pi.getId()).singleResult();
+		assertEquals("UserTask1", task.getTaskDefinitionKey());
+
+		// Set variable
+		VariableMap variables = Variables.createVariables();
+		variables.clear();
+		variables.put("FormFieldX", 3L);
+
+		// Complete task
+		taskService.complete(task.getId(), variables);
+
+		assertProcessEnded(pi.getId());
+	}
+
+}
